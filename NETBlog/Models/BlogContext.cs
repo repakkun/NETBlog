555aaa3dc25e028927c90dcc5b7172951fa40c88
rePakkun <cj.ilya@yandex.ru>
2021-11-30 19:04:50 +0300

    test

---
+++ NETBlog/Models/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NETBlog.Models
+{
+    public sealed class BlogContext : DbContext
+    {
+        /// Ссылки на таблицу
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Commentary> Commentaries { get; set; }
+        
+        // Логика взаимодействия с таблицами в БД
+        public BlogContext(DbContextOptions<BlogContext> options)  : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+  
+}
---
+++ NETBlog/Models/SampleData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+namespace NETBlog.Models
+{
+    public class SampleData
+    {
+        public static void Initialize(BlogContext context)
+        {
+            if (context.Users.Any()) return;
+            context.Users.AddRange(
+                new User
+                {
+                    Id = new Guid(),
+                    UserName = "Test",
+                    JoinDate = DateTime.Now
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
